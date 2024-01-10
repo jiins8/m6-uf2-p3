bf77ad95c5986e8474085e8d24153d52a9639bfd
Jinshuo <x6311993_b@iespoblenou.org>
2024-01-10 13:38:50 +0100

    Initial commit

---
+++ src/main/java/departments/DeptEntity.java
@@ -0,0 +1,76 @@
+package departments;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "DEPT", schema = "SCOTT", catalog = "")
+public class DeptEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "DEPTNO")
+    private byte deptno;
+    @Basic
+    @Column(name = "DNAME")
+    private String dname;
+    @Basic
+    @Column(name = "LOC")
+    private String loc;
+    @OneToMany(mappedBy = "deptByDeptno")
+    private Collection<EmpEntity> empsByDeptno;
+
+    public byte getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(byte deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DeptEntity that = (DeptEntity) o;
+
+        if (deptno != that.deptno) return false;
+        if (dname != null ? !dname.equals(that.dname) : that.dname != null) return false;
+        if (loc != null ? !loc.equals(that.loc) : that.loc != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) deptno;
+        result = 31 * result + (dname != null ? dname.hashCode() : 0);
+        result = 31 * result + (loc != null ? loc.hashCode() : 0);
+        return result;
+    }
+
+    public Collection<EmpEntity> getEmpsByDeptno() {
+        return empsByDeptno;
+    }
+
+    public void setEmpsByDeptno(Collection<EmpEntity> empsByDeptno) {
+        this.empsByDeptno = empsByDeptno;
+    }
+}
